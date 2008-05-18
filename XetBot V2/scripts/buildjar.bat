b7aca33e3a947981310fb0fdcf88a43681fc2fdc
setola <setola@96f243b3-724a-0410-bd06-1dd7ef901370>
2008-05-18 10:38:05 +0000

    cleanup some noob svn errors

--- XetBot V2/scripts/buildjar.bat
+++
@@ -1,2 +0,0 @@
-del XetBotV2.jar && jar cmf mainClass.txt XetBotV2.jar xetbotv2/*.class xetbotv2/libs/*.class xetbotv2/modules/*.class org/* com/*
-pause
--- XetBot V2/scripts/rebuild.bat
+++
@@ -1,9 +0,0 @@
-del *.class 
-del xetbotv2/*.class 
-del xetbotv2/libs/*.class 
-del xetbotv2/modules/*.class 
-
-echo ---------------------
-
-javac xetbotv2/XetBotV2Main.java
-pause
